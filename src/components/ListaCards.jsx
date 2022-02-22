e6003825743673d64d4c2486dee7c3a1b099517f
Jonas <jonasmirandaa21@gmail.com>
2022-02-22 20:13:13 -0300

    Atualizar lista de cartas

--- src/components/ListaCards.jsx
+++ src/components/ListaCards.jsx
@@ -1,20 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles.css";
 import Card from "./Card";
-import { useState } from "react";
-import { useEffect } from "react";
-import { getProfiles } from "../service/getProfiles";
 
-function ListaCards() {
-  async function atualizaProfiles() {
-    const cartas = await getProfiles();
-    setCartas(cartas);
-  }
-
-  const [cartas, setCartas] = useState([]);
-  useEffect(() => {
-    atualizaProfiles();
-  }, []);
+function ListaCards(props) {
+  const cartas = props.cartas;
   return (
     <div className="lista-cards_container">
       {cartas.map((carta) => {
