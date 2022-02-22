5b1e1a635fe5bc6a0eadfc92b6e75b14c0c89820
Jonas <jonasmirandaa21@gmail.com>
2022-02-22 20:14:08 -0300

    Atualizar componente Body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import "./styles.css";
 import { FiSearch } from "react-icons/fi";
 import ListaCards from "./ListaCards";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getProfiles } from "../service/getProfiles";
+import { filterProfiles } from "../service/filterProfiles";
 
-class Body extends Component {
-  render() {
-    return (
-      <>
+function Body() {
+  const [cartas, setCartas] = useState([]);
+  const [todasAsCartas, setTodasAsCartas] = useState([]);
+
+  useEffect(() => {
+    atualizaProfiles();
+  }, []);
+
+  async function atualizaProfiles() {
+    const cartas = await getProfiles();
+    setTodasAsCartas(cartas);
+    setCartas(cartas);
+  }
+  function filtrarProfiles(event) {
+    const valorParaFiltrar = event.target.value;
+    const cartasFiltradas = filterProfiles(todasAsCartas, valorParaFiltrar);
+    setCartas(cartasFiltradas);
+  }
+
+  return (
+    <>
+      <div className="container">
         <div className="body_barra-pesquisa">
           <div className="body_barra-pesquisa-container">
             <FiSearch color="#aaaaaa" size={20} />
-            <input type="text" />
+            <input type="text" onChange={filtrarProfiles} />
           </div>
-          <ListaCards />
+          <ListaCards cartas={cartas} />
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default Body;
