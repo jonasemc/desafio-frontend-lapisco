960b31eec8337b211aa4c58ca3756c14942fd16c
Jonas <jonasmirandaa21@gmail.com>
2022-02-22 20:10:48 -0300

    Tratar maiusculas em filtro

--- src/service/filterProfiles.js
+++ src/service/filterProfiles.js
@@ -1,8 +1,8 @@
 export const filterProfiles = (profiles, query) => {
     const result = profiles.filter((profile)=>{
         const nomeCompleto = profile.name.first + profile.name.last
-        const resultado = nomeCompleto.includes(query)
+        const resultado = nomeCompleto.toUpperCase().includes(query.toUpperCase())
         return resultado;
     })  
-    return resultado;
+    return result;
 } 
